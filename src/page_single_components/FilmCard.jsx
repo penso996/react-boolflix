3ea64e2cda56189ada4188daee997e61c05a22f4
penso996 <penso.andrea@outlook.it>
2025-02-27 18:28:07 +0100

    working on filmcard.jsx

--- src/page_single_components/FilmCard.jsx
+++ src/page_single_components/FilmCard.jsx
@@ -1,7 +1,8 @@
 // FilmCard.jsx
 
+// Import flags icons
+import Flag from 'react-world-flags';
 
-// RENDER
 const FilmCard = (props) => {
 
     const { filmCardProp } = props;
@@ -12,7 +13,15 @@ const FilmCard = (props) => {
         <div>
             <h3>{filmCardProp.title}</h3>
             <h3>{filmCardProp.original_title}</h3>
-            <h3>{filmCardProp.original_language}</h3>
+            <h3>
+                <h3>
+                    {filmCardProp.original_language === "en" ? <Flag code="GB" className='flag' /> :
+                        filmCardProp.original_language === "it" ? <Flag code="IT" className='flag' /> :
+                            filmCardProp.original_language === "es" ? <Flag code="ES" className='flag' /> :
+                                filmCardProp.original_language === "fr" ? <Flag code="FR" className='flag' /> :
+                                    "altro"}
+                </h3>
+            </h3>
             <h3>{filmCardProp.vote_average}</h3>
         </div>
     );
